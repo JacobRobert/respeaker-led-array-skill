1e77aff7349c2ab19db37f3e35cc3dbf6bb23839
JacobRobert <jacob.robert.005@gmail.com>
2021-08-27 17:06:02 -0500

    Added pixels classes and set up settingsmeta file

--- __init__.py
+++ __init__.py
@@ -1,9 +1,13 @@
 from mycroft import MycroftSkill, intent_file_handler
+from four_mic_hat_interfaces.pixels import Pixels
 
 
 class RespeakerLedArray(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+
+    def initialize(self):
+        LED_pattern = self.settings.get('LED_pattern')
 
     @intent_file_handler('array.led.respeaker.intent')
     def handle_array_led_respeaker(self, message):
